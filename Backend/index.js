737b7fee6261758e1a105b04dd4f3a8d5b05fd32
Sourav Vijay <souravvijay637@gmail.com>
2026-05-11 12:12:14 +0530

    fix: update google auth callback to render url

--- Backend/index.js
+++ Backend/index.js
@@ -99,7 +99,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "https://habitster.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
